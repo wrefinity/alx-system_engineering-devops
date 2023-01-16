b87a00b86721556af106ae50b7305bef8e11a317
root <wrefordmessi@gmail.com>
2023-01-16 07:25:49 -0800

    dictionary get information

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,26 +5,21 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = '{}users'.format(url)
-    res = requests.get(user)
-    json_o = res.json()
-    d_task = {}
-    for user in json_o:
-        name = user.get('username')
-        user_id = user.get('id')
-        todos = '{}todos?userId={}'.format(url, user_id)
-        res = requests.get(todos)
-        tasks = res.json()
-        l_task = []
-        for task in tasks:
-            dictn = {"username": name,
-                         "task": task.get('title'),
-                         "completed": task.get('completed')}
-            l_task.append(dictn)
-
-        d_task[str(user_id)] = l_task
-    filename = 'todo_all_employees.json'
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
+    us = requests.get(url, verify=False).json()
+    undoc = {}
+    udoc = {}
+    for user in us:
+        uid = user.get("id")
+        udoc[uid] = []
+        undoc[uid] = user.get("username")
+    url = "https://jsonplaceholder.typicode.com/todos"
+    todo = requests.get(url, verify=False).json()
+    [udoc.get(t.get("userId")).append({"task": t.get("title"),
+                                       "completed": t.get("completed"),
+                                       "username": undoc.get(
+                                               t.get("userId"))})
+     for t in todo]
+    with open("todo_all_employees.json", 'w') as jsf:
+        json.dump(udoc, jsf)
